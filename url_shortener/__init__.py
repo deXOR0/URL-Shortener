4678683c5e0bc7ba9402f6503dcc04c96b27cdda
deXOR0 <awesaatyanta.p@gmail.com>
2021-01-23 11:00:23 +0700

    Fixed requirements

--- url_shortener/__init__.py
+++ url_shortener/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask #Basic flask dependecies
 from flask_sqlalchemy import SQLAlchemy #Database connection and ORM
 from flask_bcrypt import Bcrypt #Hashing and CSRF token
-from flask_login import LoginManager #Login Manager
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager 
 
@@ -16,7 +15,6 @@ manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
 bcrypt = Bcrypt(app)
-login_manager = LoginManager(app)
 # Uncomment the two lines below to implement default login page
 # login_manager.login_view = 'login'
 # login_manager.login_message_category = 'info'
